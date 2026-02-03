bf2dd7fbabb0b91a210046e88ced47d4e4ec72e7
Himanshu Kumar <himanshukumar236@lpu.in>
2026-02-03 23:42:40 +0530

    aa

---
+++ Basics/palindromelinekdlist.cpp
@@ -0,0 +1,61 @@
+// 234. Palindrome Linked List
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,2,1]
+// Output: true
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: false
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == nullptr) return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = nullptr;
+        while (slow) {
+            ListNode* nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;
+        while (second) {
+            if (first->val != second->val) return false;
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
