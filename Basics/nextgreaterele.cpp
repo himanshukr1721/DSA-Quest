d164c33d8f837a1c2be6584eecb0bc543494ea69
Himanshu Kumar <himanshukumar236@lpu.in>
2026-07-13 23:47:55 +0530

    woah

---
+++ Basics/nextgreaterele.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& a) {
+        int n = a.size();
+        vector<int>v(n,-1);
+
+        stack<int>st;
+        for(int i = 2*n - 1; i >= 0; i--)
+        {
+            while(!st.empty() && (a[i%n] >= st.top()))
+            {
+                st.pop();
+            }
+            if(!st.empty() && (i < n))
+            {
+                v[i] = st.top();
+            }
+
+            st.push(a[i%n]);
+        }
+
+        return v;
+    }
+};
+
+// 503. Next Greater Element II
+// Solved
+// Medium
+// Topics
+// premium lock icon
+// Companies
+// Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+// The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,1]
+// Output: [2,-1,2]
+// Explanation: The first 1's next greater number is 2; 
+// The number 2 can't find next greater number. 
+// The second 1's next greater number needs to search circularly, which is also 2.
+// Example 2:
+
+// Input: nums = [1,2,3,4,3]
+// Output: [2,3,4,-1,4]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -109 <= nums[i] <= 109
